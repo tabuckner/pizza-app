bc1520aa5de06dc8de16527f916986d36acfbf43
Taylor Buckner <tbuckner@theappraisallane.com>
2018-05-25 12:32:20 -0500

    Material Themed Shell

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from "@angular/core";
 
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatCardModule } from '@angular/material';
 import { ProfileComponent } from './components/profile/profile.component'
 import { CommonModule } from "@angular/common";
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
   ],
   exports: [
     MatToolbarModule,
     ProfileComponent
   ],
-  declarations: [ProfileComponent]
+  declarations: [
+    ProfileComponent,
+  ]
 })
 export class SharedModule {
 
